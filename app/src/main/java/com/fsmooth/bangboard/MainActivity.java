af359fc0e12163eab1c0654fa6ab155ccd5c45c7
fSmooth <fSmoothdev@gmail.com>
2017-01-22 12:34:31 +0100

    Estructura en tabs
    Definida la estructura de tabs.

--- app/src/main/java/com/fsmooth/bangboard/MainActivity.java
+++ app/src/main/java/com/fsmooth/bangboard/MainActivity.java
@@ -2,12 +2,35 @@ package com.fsmooth.bangboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    TabHost tabs;
+    TabHost.TabSpec spec;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tabs = (TabHost) findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        spec = tabs.newTabSpec("tabCharacter");
+        spec.setContent(R.id.tabCharacter);
+        spec.setIndicator("",
+                getResources().getDrawable(R.drawable.ic_character));
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("tabEquipment");
+        spec.setContent(R.id.tabEquipment);
+        spec.setIndicator("",
+                getResources().getDrawable(R.drawable.ic_sword));
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+
+
     }
 }
