826677cf2fa12b077deeb6502bbfff113dd4d8cf
fSmooth <fSmoothdev@gmail.com>
2017-02-06 09:06:15 +0100

    Pequeños arreglos

--- app/src/main/java/com/fsmooth/bangboard/MainActivity.java
+++ app/src/main/java/com/fsmooth/bangboard/MainActivity.java
@@ -4,7 +4,6 @@ package com.fsmooth.bangboard;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
